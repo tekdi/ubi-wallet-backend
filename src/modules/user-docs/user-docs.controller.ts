78b9fbce817cac7d89af6ec7661ffd25cf393322
Vidyaranya-Gavai-TTPL <vidyaranya.gavai@tekditechnologies.com>
2024-12-12 17:03:18 +0530

    API for deleting document (#25)
    * API for deleting document
    
    * Sonar issues fix: v1

--- src/modules/user-docs/user-docs.controller.ts
+++ src/modules/user-docs/user-docs.controller.ts
@@ -1,31 +1,52 @@
-import { Body, Controller, Get, Post, Req, Res, UnauthorizedException, UploadedFile, UseGuards,UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Req,
+  Res,
+  UnauthorizedException,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { UserDocsService } from './user-docs.service';
 import { CreateUserDocDto } from './dto/create-user-doc.dto';
-import { ApiBasicAuth, ApiConsumes, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiBasicAuth, ApiConsumes, ApiOperation } from '@nestjs/swagger';
 import { UserDoc } from './entity/user-doc.entity';
 import { AuthGuard } from '../auth/auth.guard';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { Express,Response } from 'express';
+import { Express, Response } from 'express';
 import { AuthenticatedRequest } from '../../types/authenticated-request.interface';
 
 @Controller('user-docs')
 export class UserDocsController {
   constructor(private readonly userDocsService: UserDocsService) {}
 
-
   @UseInterceptors(FileInterceptor('file'))
   @Post()
   @UseGuards(AuthGuard)
   @ApiOperation({ summary: 'Create a new user document' })
   @ApiConsumes('multipart/form-data')
-  async create(@Req() req: AuthenticatedRequest,@UploadedFile() vcfile: Express.Multer.File,@Body() createUserDocDto: CreateUserDocDto,@Res() res: Response){
+  async create(
+    @Req() req: AuthenticatedRequest,
+    @UploadedFile() vcfile: Express.Multer.File,
+    @Body() createUserDocDto: CreateUserDocDto,
+    @Res() res: Response,
+  ) {
     const user = req?.user;
     if (!user) {
       throw new UnauthorizedException('User is not authenticated');
     }
     const ssoId = user.keycloak_id;
 
-    let uploadedDoc = await this.userDocsService.create(createUserDocDto,vcfile,ssoId)
+    let uploadedDoc = await this.userDocsService.create(
+      createUserDocDto,
+      vcfile,
+      ssoId,
+    );
     return res.status(uploadedDoc.statusCode).json({ ...uploadedDoc });
   }
 
@@ -38,8 +59,27 @@ export class UserDocsController {
     if (!user) {
       throw new UnauthorizedException('User is not authenticated');
     }
-    const ssoId = user.keycloak_id; 
+    const ssoId = user.keycloak_id;
 
     return this.userDocsService.fetchBySsoId(ssoId);
+  }
+
+  @Delete('/delete/:doc_id')
+  @UseGuards(AuthGuard)
+  @ApiOperation({ summary: 'Delete a document' })
+  @ApiBasicAuth('access-token')
+  async deleteDoc(
+    @Req() req: AuthenticatedRequest,
+    @Param() doc_id: any,
+    @Res() res: Response,
+  ) {
+    const user = req?.user;
+    if (!user) {
+      throw new UnauthorizedException('User is not authenticated');
+    }
+    const ssoId = user.keycloak_id;
+
+    const response = await this.userDocsService.delete(doc_id.doc_id, ssoId);
+    return res.status(response.statusCode).json(response);
   }
 }
--- src/modules/user-docs/user-docs.service.ts
+++ src/modules/user-docs/user-docs.service.ts
@@ -1,20 +1,25 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  HttpStatus,
+  Injectable,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UserDoc } from './entity/user-doc.entity';
 import { CreateUserDocDto } from './dto/create-user-doc.dto';
 import { ErrorResponse } from 'src/common/responses/error-response';
 import { SuccessResponse } from 'src/common/responses/success-response';
-import axios from 'axios'
+import axios from 'axios';
 import { v4 as uuidv4 } from 'uuid';
 @Injectable()
 export class UserDocsService {
   constructor(
     @InjectRepository(UserDoc)
-    private userDocsRepository: Repository<UserDoc>,
+    private readonly userDocsRepository: Repository<UserDoc>,
   ) {}
 
-  async create(createUserDocDto: CreateUserDocDto, vcfile:any, ssoId:string){
+  async create(createUserDocDto: CreateUserDocDto, vcfile: any, ssoId: string) {
     try {
       // if(createUserDocDto.doc_id != undefined){
       //   try {
@@ -31,15 +36,15 @@ export class UserDocsService {
       //     }
       //     throw error;
       //   }
-      // }else 
-      if(vcfile != undefined){
+      // }else
+      if (vcfile != undefined) {
         const vcJsonString = vcfile.buffer.toString();
         const vcJsonData = JSON.parse(vcJsonString);
         createUserDocDto.doc_id = vcJsonData.id;
         createUserDocDto.doc_data = vcJsonData;
         createUserDocDto.issuer = vcJsonData.issuer;
       }
-      if(createUserDocDto.doc_id == undefined ){
+      if (createUserDocDto.doc_id == undefined) {
         let uniqueUUID;
         let isUnique = false;
         while (!isUnique) {
@@ -64,30 +69,32 @@ export class UserDocsService {
         });
       }
       const checkAadhaarExist = await this.userDocsRepository.findOne({
-        where: { sso_id: ssoId , doc_subtype:'aadhaar' },
+        where: { sso_id: ssoId, doc_subtype: 'aadhaar' },
       });
-      if(checkAadhaarExist && createUserDocDto.doc_subtype==="aadhaar" ) {
+      if (checkAadhaarExist && createUserDocDto.doc_subtype === 'aadhaar') {
         return new ErrorResponse({
           statusCode: 409,
           errorMessage: `Aadhaar already added to wallet`,
         });
       }
-      if (!checkAadhaarExist && createUserDocDto.doc_subtype!=="aadhaar") {
+      if (!checkAadhaarExist && createUserDocDto.doc_subtype !== 'aadhaar') {
         return new ErrorResponse({
           statusCode: 400,
           errorMessage: `Please add Aadhaar first before adding any other document to wallet`,
         });
       }
-      
-      const userDoc = this.userDocsRepository.create({...createUserDocDto,sso_id:ssoId});
+
+      const userDoc = this.userDocsRepository.create({
+        ...createUserDocDto,
+        sso_id: ssoId,
+      });
       await this.userDocsRepository.save(userDoc);
       return new SuccessResponse({
         statusCode: 200, // HTTP OK status
         message: 'DOCUMENTS_ADDED_SUCCESSFULLY',
         data: userDoc,
       });
-    }
-    catch(error) {
+    } catch (error) {
       return new ErrorResponse({
         statusCode: 500, // Internal server error status
         errorMessage: `FAILED_TO_CREATE_DOCUMENT - ${error.message}`,
@@ -106,21 +113,72 @@ export class UserDocsService {
         method: 'get',
         maxBodyLength: Infinity,
         url: apiUrl,
-        headers: { 
+        headers: {
           Accept: 'application/json',
         },
       });
-  
+
       if (!response.data) {
         throw new NotFoundException(`Document with doc_id ${doc_id} not found`);
       }
-  
+
       return response.data;
     } catch (error: any) {
       if (error.response && error.response.status === 404) {
         throw new NotFoundException(`Document with doc_id ${doc_id} not found`);
       }
-      throw new Error(`Failed to fetch doc_data for doc_id: ${doc_id} - ${error.message}`);
+      throw new Error(
+        `Failed to fetch doc_data for doc_id: ${doc_id} - ${error.message}`,
+      );
     }
+  }
+
+  async delete(doc_id: string, sso_id: string) {
+    // Check if document exists or not, if not then send erorr response
+    const existingDoc = await this.userDocsRepository.findOne({
+      where: {
+        doc_id: doc_id,
+      },
+    });
+
+    if (!existingDoc) {
+      Logger.error(`Document with id ${doc_id} does not exists`);
+      return new ErrorResponse({
+        statusCode: 400,
+        errorMessage: `Document with id ${doc_id} does not exists`,
+      });
+    }
+
+    // Check if logged in user is allowed to delete this document or not
+    if (existingDoc.sso_id !== sso_id)
+      return new ErrorResponse({
+        statusCode: HttpStatus.UNAUTHORIZED,
+        errorMessage:
+          'You are not authorized to modify or delete this resourse',
+      });
+
+    // Delete the document
+    const queryRunner =
+      this.userDocsRepository.manager.connection.createQueryRunner();
+    await queryRunner.connect();
+    try {
+      await queryRunner.startTransaction();
+      await queryRunner.manager.remove(existingDoc);
+      await queryRunner.commitTransaction();
+    } catch (error) {
+      Logger.error('Error while deleting the document: ', error);
+      await queryRunner.release();
+      return new ErrorResponse({
+        statusCode: 500,
+        errorMessage: `Error while deleting the document: ${error}`,
+      });
+    } finally {
+      await queryRunner.release();
+    }
+
+    return new SuccessResponse({
+      statusCode: 200,
+      message: 'Document deleted successfully',
+    });
   }
 }
